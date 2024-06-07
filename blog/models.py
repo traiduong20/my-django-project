cf2c841ff1c55776890a62acc6f49e2c5a57870a
traiduong20 <traiduong.24hdev@gmail.com>
2024-06-07 11:14:14 +0700

    update css

--- blog/models.py
+++ blog/models.py
@@ -2,13 +2,13 @@ from django.conf import settings
 from django.db import models
 from django.utils import timezone
 
-
 class Post(models.Model):
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     text = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
     published_date = models.DateTimeField(blank=True, null=True)
+    link = models.CharField(max_length=200, blank=True, null=True)  # Add the 'link' field
 
     def publish(self):
         self.published_date = timezone.now()
